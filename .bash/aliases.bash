a009d64127fdbeb81c42d3cc03cd73df6e7d5ba8
Marten Vohrmann <mvohrmann@cit-ec.uni-bielefeld.de>
2017-11-16 11:05:28 +0100

    separated aliases in bashrc

---
+++ .bash/aliases.bash
@@ -0,0 +1,11 @@
+# Aliases 
+
+alias fl="find . -iname "
+alias l="ls -l"
+alias ll="ls -l --color"
+alias lh="ls -lha"
+alias c="cat"
+alias v="vim"
+alias g="gedit"
+alias ..="cd ../.."
+alias now="date +%s"
